e51901cb9bec4ff47b96e508aa6908654140434f
Todor Krastev <to6esko69@gmail.com>
2016-06-04 18:57:13 +0300

    add routes

--- yelp/src/containers /App/App.js
+++ yelp/src/containers /App/App.js
@@ -1,18 +1,29 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
+import React, {PropTypes} from 'react';
+import {Router} from 'react-router';
+import ReactDOM from 'react-dom';
+
 
 import styles from './styles.module.css'
 
-const App = React.createClass({
-  render: function() {
-    return (
-      <div className={styles.wrapper}>
-        <h1>
-          <i className="fa fa-star"></i>
-          Environment: {__NODE_ENV__}</h1>
-      </div>
-    )
-  }
-});
+class App extends React.Component{
+    static propTypes = {
+        routes: PropTypes.object.isRequired,
+        history: PropTypes.object.isRequired
+    }
+    
+    get content() {
+        return (<Router
+            routes={this.props.routes}
+            history={this.props.history}/>
+        )
+    }
+    
+    render() {
+        return (
+            <div style={{ height:'100%'}}>
+                {this.content}
+            </div>)
+    }
+}
 
 module.exports = App;
