124ae88f42e0207b6566da0fd0975345a6adea84
Todor Krastev <to6esko69@gmail.com>
2016-06-04 16:39:41 +0300

    add css stars

--- yelp/src/app.js
+++ yelp/src/app.js
@@ -1,17 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import styles from './styles.module.css';
 import './app.css'
+import styles from './styles.module.css'
+
+import 'font-awesome/css/font-awesome.css'
 
 const App = React.createClass({
-    render: function () {
-        return (
-            <div className={styles.wrapper}>
-                <h1>Environment: {__NODE_ENV__}</h1>
-            </div>
-        );
-    }
+  render: function() {
+    return (
+      <div className={styles.wrapper}>
+        <h1>
+          <i className="fa fa-star"></i>
+          Environment: {__NODE_ENV__}
+            </h1>
+      </div>
+    )
+  }
 });
 
 const mountNode = document.querySelector('#root');
--- yelp/webpack.config.js
+++ yelp/webpack.config.js
@@ -2,17 +2,18 @@ const NODE_ENV = process.env.NODE_ENV;
 const dotenv = require('dotenv');
 
 const webpack = require('webpack');
-const fs = require('fs');
-const path = require('path'),
-    join = path.join,
-    resolve = path.resolve;
+const fs      = require('fs');
+const path    = require('path'),
+      join    = path.join,
+      resolve = path.resolve;
+
 const getConfig = require('hjs-webpack');
 
 
 
 
 
-const isDev = NODE_ENV === 'development';
+const isDev  = NODE_ENV === 'development';
 // alternatively, we can use process.argv[1]
 // const isDev = (process.argv[1] || '')
 //                .indexOf('hjs-dev-server') !== -1;
@@ -21,7 +22,7 @@ const isDev = NODE_ENV === 'development';
 
 const root = resolve(__dirname);
 const src = join(root, 'src');
-const mudules = join(root, 'node_modules');
+const modules = join(root, 'node_modules');
 const dest = join(root, 'dist');
 
 
@@ -33,37 +34,41 @@ var config = getConfig({
 })
 
 
+// ENV variables
 const dotEnvVars = dotenv.config();
 const environmentEnv = dotenv.config({
-    path: join(root, 'config', `${NODE_ENV}.config.js`),
-    silent:true,
+  path: join(root, 'config', `${NODE_ENV}.config.js`),
+  silent: true,
 });
-const envVariables=Object.assign({},dotEnvVars,environmentEnv);
+const envVariables =
+    Object.assign({}, dotEnvVars, environmentEnv);
 
-const defines = Object.keys(envVariables).reduce((memo, key) => {
+const defines =
+  Object.keys(envVariables)
+  .reduce((memo, key) => {
     const val = JSON.stringify(envVariables[key]);
     memo[`__${key.toUpperCase()}__`] = val;
     return memo;
-}, {
-        __NODE_ENV__: JSON.stringify(NODE_ENV)        
-});
-
-
+  }, {
+    __NODE_ENV__: JSON.stringify(NODE_ENV)
+  });
 
 config.plugins = [
-    new webpack.DefinePlugin(defines)
+  new webpack.DefinePlugin(defines)
 ].concat(config.plugins);
+// END ENV variables
 
 
 
 
-const cssModulesNames = `${isDev? '[path][name]__[local]__' : ''}[hash:base64:5]`;
+
+// CSS modules
+const cssModulesNames = `${isDev ? '[path][name]__[local]__' : ''}[hash:base64:5]`;
 
 const matchCssLoaders = /(^|!)(css-loader)($|!)/;
 
 const findLoader = (loaders, match) => {
-  const found = loaders.filter(l => l &&
-      l.loader && l.loader.match(match));
+  const found = loaders.filter(l => l && l.loader && l.loader.match(match))
   return found ? found[0] : null;
 }
 // existing css loader
@@ -73,18 +78,28 @@ const cssloader =
 const newloader = Object.assign({}, cssloader, {
   test: /\.module\.css$/,
   include: [src],
-  loader: cssloader.loader
-    .replace(matchCssLoaders,
-    `$1$2? modules & localIdentName=${cssModulesNames }$3`)
+  loader: cssloader.loader.replace(matchCssLoaders, `$1$2?modules&localIdentName=${cssModulesNames}$3`)
 })
 config.module.loaders.push(newloader);
-cssloader.test =
-  new RegExp(`[^module]${cssloader.test.source }`)
+cssloader.test = new RegExp(`[^module]${cssloader.test.source}`)
 cssloader.loader = newloader.loader
 
+config.module.loaders.push({
+  test: /\.css$/,
+  include: [modules],
+  loader: 'style!css'
+})
+// CSS modules
+
+
+
+// postcss
 config.postcss = [].concat([
-    require('precss')({}),
-    require('autoprefixer')({}),
-    require('cssnano')({})
+  require('precss')({}),
+  require('autoprefixer')({}),
+  require('cssnano')({})
 ])
+// END postcss
+
+
 module.exports = config;
