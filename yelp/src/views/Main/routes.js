0955eb7a429b0275523bc92fefbcf74332fede09
Todor Krastev <to6esko69@gmail.com>
2016-06-06 08:41:22 +0300

    add css mobile

--- yelp/src/views/Main/routes.js
+++ yelp/src/views/Main/routes.js
@@ -1,12 +1,15 @@
 import React from 'react';
-import {Route} from 'react-router';
+import {Route,IndexRoute} from 'react-router';
 import Container from './Container';
 import Map from './Map/Map';
+import Detail from './Detail/Detail';
 
 export const makeMainRoutes = () => {
     return (
         <Route path="/" component={Container}>
+            <IndexRoute component={Map}/>
             <Route path="map" component={Map}/>
+            <Route path="detail/:placeId" component={Detail}/>
             </Route>
     )
 }
