08e3d8350c6e7bf79f7730352cb7556348eef8d3
Todor Krastev <to6esko69@gmail.com>
2016-06-08 18:51:56 +0300

    fix item.js

--- yelp/src/components /Listing/Item.js
+++ yelp/src/components /Listing/Item.js
@@ -1,6 +1,6 @@
 import React, {PropTypes as T} from 'react';
 import classnames from 'classnames';
-import Rating from 'components/Rating/Rating';
+import Rating from '../Rating/Rating';
 import styles from './styles.module.css';
 
 export class Item extends React.Component{
